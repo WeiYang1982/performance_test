0fea9ba116aecb455567d6d6572fd8aef13a233d
wei.yang <wei.yang@cyclone-robotics.com>
2022-07-19 18:18:36 +0800

    计算接口吞吐量

--- src/utils/parse_jtl_report.py
+++ src/utils/parse_jtl_report.py
@@ -11,6 +11,21 @@ import pandas as pd
 import urllib.parse
 
 
+def count_throughput(dataframe):
+    """
+    计算吞吐量 吞吐量 = 请求数量 / 请求耗时
+    若只有一个请求时 请求耗时 = elapsed 列的值
+    """
+    exec_time = dataframe['timeStamp'].max() - dataframe['timeStamp'].min()
+    if not exec_time == 0:
+        throughput = 1000 * len(dataframe) / exec_time
+    else:
+        avg_exec_time = dataframe['elapsed'].mean()
+        throughput = 1000 * len(dataframe) / avg_exec_time
+    dataframe['throughput'] = throughput
+    return dataframe
+
+
 class SamplesParser:
     # def __init__(self):
 
@@ -22,10 +37,13 @@ class SamplesParser:
     def analytics_sample(self, data_frame):
         # 取path
         data_frame['URL'] = data_frame['URL'].apply(lambda x: urllib.parse.urlparse(x).path if pd.isnull(x) == False else x)
+        data_frame = data_frame.groupby('label', as_index=True).apply(lambda x: count_throughput(x))
         data = data_frame.groupby(["label", "URL"], as_index=True).agg(
-            {"elapsed": [pd.DataFrame.mean, pd.DataFrame.max, pd.DataFrame.min, len]})
-        data['throughput'] = data_frame.groupby('label', as_index=True).apply(
-            lambda x: len(data_frame) / (x['timeStamp'].max() - x['timeStamp'].min())) * 1000
+            {"elapsed": [pd.DataFrame.mean, pd.DataFrame.max, pd.DataFrame.min, len], "throughput": [pd.DataFrame.mean]})
+        # data['throughput'] = data_frame.groupby('label', as_index=True).apply(
+        #     lambda x: len(data_frame) / (x['timeStamp'].max() - x['timeStamp'].min())) * 1000
+
+        # data['throughput'] = data_frame.groupby('throughput').agg()
         success_data = data_frame[data_frame['success'] == True]
         success_group = success_data.groupby('label').agg({'label': len})  #
         data['success_rate'] = success_group['label'] / data['elapsed']['len'] * 100
@@ -48,7 +66,9 @@ if __name__ == '__main__':
     # for d in data:
     #     print(d)
     #     print("***")
-    file = "D:\\Code\\python_project\\performance_test\\tests\\report\\jmeter\\result.csv"
-    da = pd.read_csv(file, encoding='utf-8', header=0)
-    print(da.values.tolist()[0][1])
+    file = "D:\\Code\\python_project\\performance_test\\report\\jmeter\\analytics_1_20.jtl"
+    p = SamplesParser()
+    data = p.get_samples(file)
+    cases = p.analytics_sample(data)
+    print(cases)
     pass
