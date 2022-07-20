8485410bef297ed80654a51a9e87aa27b0b620f1
wei.yang <wei.yang@cyclone-robotics.com>
2022-07-20 10:21:46 +0800

    修改登录界面部分逻辑

--- src/js/login.js
+++ src/js/login.js
@@ -11,11 +11,19 @@ async function login(passContext) {
     // networkidle0 - 不再有网络连接时触发（至少500毫秒后）
     // networkidle2 - 只有2个网络连接时触发（至少500毫秒后）
     await page.goto(login_url, {waitUntil: 'networkidle0'});
+    // 开启企业内部登录时，选择系统账号登录
+    try {
+        await page.click("input[type=radio][value='0']")
+    }catch (e) {
+        console.log(e)
+    }
     const username = process.env.username;
     const password = process.env.password;
     await page.type('input[class=ant-input][type=text]', username)
     await page.type('input[class=ant-input][type=password]', password)
-    await page.click('button[type=\'submit\']', {waitUntil: 'networkidle0'})
+    // await page.click('button[type=\'submit\']', {waitUntil: 'networkidle0'})
+    // 统一使用回车键进行登录
+    await page.keyboard.press('Enter')
     // 等待页面加载完毕
     await page.waitForNavigation({waitUntil: 'networkidle0',});
 }
